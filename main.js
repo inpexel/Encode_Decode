d741ad5f2c482e8179ab7fffb79b23d76daf6db3
Vishal Deshwal <inpexel@gmail.com>
2022-09-20 10:17:13 +0530

    cleaning the code

--- main.js
+++ main.js
@@ -1,54 +1,37 @@
-const generate =  document.querySelector('#generate');
-const errorText = document.querySelector('#error');
-
-
-function getPassword(){
+(function(){
+    const generate =  document.querySelector('#generate');
+    const errorText = document.querySelector('#error');
     const password = document.querySelector('#enterPass');
-    console.log(password.value);
-    return password.value;
-}
 
-function encode(data){
-    let fullData =[];
-    // let output = [];
-    for(let i=0; i<data.length; i++){
-        let charEncode = data.charCodeAt(i);
-        console.log(charEncode);
-        fullData.push(charEncode);
+    function encode(data){
+        let fullData =[];
+        for(let i=0; i<data.length; i++){
+            let charEncode = data.charCodeAt(i);
+            fullData.push(charEncode);
+        }
+
+        let newFullData = fullData.map(data => (data / 2) + 3 +(data / 3) * 1.5);
+        let recoverFullData = newFullData.map(data => (data * 2) - 18);
+        let numToString = newFullData.map(data => String.fromCharCode(data));
+        let output = numToString.join('');
+        return output;
     }
-    let newFullData = fullData.map(data => (data / 2) + 3 +(data / 3) * 1.5);
-    let recoverFullData = newFullData.map(data => (data * 2) - 18);
-    let numToString = newFullData.map(data => String.fromCharCode(data));
-    let output = numToString.join('')
 
-    
-    // output.push(String.fromCharCode(newFullData));
-    console.log(fullData)
-    console.log(newFullData)
-    console.log(output)
-    return output;
-}
+    function encodeResult(data){
+        const result =  document.querySelector('#result');
+        result.value = data;
+    }
 
-function encodeResult(data){
-    const result =  document.querySelector('#result');
-    result.value = data;
-}
+    function finalData(){
+        if(password.value < 1){
+            errorText.innerText = 'Type Something ';
+        }
+        else{
+            let encodedData = encode(password.value);
+            encodeResult(encodedData);
+            errorText.innerText = '';
+        }
+    }
 
-function decodeResult(data){
-    String.fromCharCode(data);
-    console.log(data);
-}
-
-function finalData(){
-    let password = getPassword();
-    let encodedData = encode(password);
-    encodeResult(encodedData);
-}
-
-let text = String.fromCharCode(68, 61, 66, 61, 57, 63);
-console.log(text);
-
-
-
-
-generate.addEventListener('click', finalData);
+    generate.addEventListener('click', finalData);
+})()
